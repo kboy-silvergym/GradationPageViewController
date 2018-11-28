3737e627c2a0f1c7af6f484bb3509c9eaa2118a9
Kei Fujikawa <fujikei22funk@gmail.com>
2018-11-28 10:23:39 +0900

    add vc

---
+++ GradationPageViewController/GradationPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GradationPageViewController.swift
+//  GradationPageViewController
+//
+//  Created by Kei Fujikawa on 2018/11/28.
+//  Copyright © 2018 kboy. All rights reserved.
+//
+
+import UIKit
+
+class GradationPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
